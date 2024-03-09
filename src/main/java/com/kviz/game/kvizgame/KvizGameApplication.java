741e6834aced91f68117c8c600aeafcd1ce7e7ad
DanilaShikalov <dan.shikalov@gmail.com>
2024-03-09 20:18:41 +0300

    update check

--- src/main/java/com/kviz/game/kvizgame/KvizGameApplication.java
+++ src/main/java/com/kviz/game/kvizgame/KvizGameApplication.java
@@ -10,61 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KvizGameApplication implements CommandLineRunner {
-    @Autowired
-    private QuestionRepository questionRepository;
-    @Autowired
-    private UserRepository userRepository;
+public class KvizGameApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KvizGameApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        questionRepository.save(Question.builder()
-                .id(null)
-                .question("Что?1")
-                .answerA("a")
-                .answerB("b")
-                .answerC("c")
-                .answerD("d")
-                .currentAnswer("a")
-                .price(1)
-                .prize("приз1")
-                .build());
-        questionRepository.save(Question.builder()
-                .id(null)
-                .question("Что?2")
-                .answerA("a")
-                .answerB("b")
-                .answerC("c")
-                .answerD("d")
-                .currentAnswer("a")
-                .price(1)
-                .prize("приз2")
-                .build());
-        questionRepository.save(Question.builder()
-                .id(null)
-                .question("Что?3")
-                .answerA("a")
-                .answerB("b")
-                .answerC("c")
-                .answerD("d")
-                .currentAnswer("a")
-                .price(1)
-                .prize("приз3")
-                .build());
-        questionRepository.save(Question.builder()
-                .id(null)
-                .question("Что?4")
-                .answerA("a")
-                .answerB("b")
-                .answerC("c")
-                .answerD("d")
-                .currentAnswer("a")
-                .price(1)
-                .prize("приз4")
-                .build());
     }
 }
